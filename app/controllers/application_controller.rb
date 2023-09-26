2513be36c03737b305515660f7ecd9b4e060bf0f
kanerin <kanerin1004@gmail.com>
2023-09-26 18:43:17 +0900

    Temporary commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+    def test
+        render html: "テスト：兼城うんこ！"
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root to: 'users#index'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
